a8a893ca5a089656674209da08f52b98367b3248
hoangtmm <hoangtm06@gmail.com>
2025-06-26 19:31:19 +0700

    fix: allow Render host in Vite config

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     alias: {
       // eslint-disable-next-line no-undef
       '@': path.resolve(__dirname, './src'),
+      allowedHosts: ['mitsubishi-satsco.onrender.com']
 
     }
   },
